967cbedbdbbed3508f1f65255b3bef4dcf6a27bd
Grigorij-Dudnik <dudnikgrv@gmail.com>
2025-10-07 15:58:21 +0200

    switch to langchain

--- src/LLMAgent.py
+++ src/LLMAgent.py
@@ -3,25 +3,16 @@ from smolagents import ToolCallingAgent, LiteLLMModel, TransformersModel
 #from phoenix.otel import register
 from dotenv import find_dotenv, load_dotenv
 from time import perf_counter
-from pydantic_ai import Agent, BinaryContent
 import cv2
-
-from haystack import Pipeline
-from haystack.components.generators.chat import OpenAIChatGenerator
-from haystack.dataclasses import ChatMessage, ByteStream, ImageContent
 import base64
-
-
-load_dotenv(find_dotenv())
-from langfuse import get_client
-langfuse = get_client()
-
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.chat_models import init_chat_model
 
 
 dotenv_time = perf_counter()
 # langfuse or arize
 # register()
-Agent.instrument_all()
+
 
 
 tool_calling_agent_system_prompt = "You are mobile robot with two arms."
@@ -31,42 +22,38 @@ tool_calling_agent_system_prompt = "You are mobile robot with two arms."
 class LLMAgent():
     def __init__(self, model, tools, system_prompt=None, main_camera_usb_port=None):
         system_prompt = system_prompt or tool_calling_agent_system_prompt
-        #super().__init__(model=model, tools=tools, system_prompt=system_prompt)
-        self.message_history = []
+        self.llm = init_chat_model(model)
+        self.message_history = [SystemMessage(content=system_prompt)]
         # cameras
         self.main_camera = cv2.VideoCapture(main_camera_usb_port) if main_camera_usb_port else None
-
-        self.generator = OpenAIChatGenerator(model="gpt-4o-mini")
 
     def capture_image(self):
         _, frame = self.main_camera.read()
         _, buffer = cv2.imencode('.jpg', frame)
-        image_bytes = buffer.tobytes()
-        base64_image = base64.b64encode(image_bytes).decode("utf-8")
-        return base64_image
+        return buffer.tobytes()
 
     def go(self):
         while True:
             if self.main_camera:
-                base64_image = self.capture_image()
-                image_content = ImageContent(
-                    base64_image=base64_image,
-                    mime_type="image/jpeg",
-                    detail="low"
-                )
-                message = ChatMessage.from_user(
-                    text="Here is the current view from your main camera. Tell what you can see here",
-                    meta={
-                        "images": [image_content]
-                    }
+                image_bytes = self.capture_image()
+                image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+                
+                message = HumanMessage(
+                    content=[
+                        {"type": "text", "text": "Here is the current view from your main camera. Tell what you can see here"},
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": f"data:image/jpeg;base64,{image_base64}"}
+                        }
+                    ]
                 )
             else:
-                message = ChatMessage.from_user("What is your next action?")
+                message = HumanMessage(content="What is your next action?")
             self.message_history.append(message)
-            response = self.generator.run(messages=self.message_history)
-            reply = response["replies"][0]
-            self.message_history.append(reply)
-            print(reply._content)
+            response = self.llm.invoke(self.message_history)
+            self.message_history.append(response)
+            print(response.content)
+            
 
 if __name__ == "__main__":
 
@@ -74,10 +61,10 @@ if __name__ == "__main__":
         print("Doing nothing...")
         return "Doing nothing."
     agent = LLMAgent(
-        model="openai:gpt-4.1-nano",
+        model="gpt-4.1-nano",
         tools=[
             do_nothing,
-        ]
+        ],
     )
     result = agent.go()
     print(result)
