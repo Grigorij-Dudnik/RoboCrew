b07e9acb929588c6034ad1db762bcdc11de7a125
Grigorij-Dudnik <dudnikgrv@gmail.com>
2025-10-07 16:43:22 +0200

    switch to langchain

--- src/LLMAgent.py
+++ src/LLMAgent.py
@@ -13,19 +13,17 @@ from langchain.chat_models import init_chat_model
 load_dotenv(find_dotenv())
 
 
-
-
-
 class LLMAgent():
     def __init__(self, model, tools, system_prompt=None, main_camera_usb_port=None):
         base_system_prompt = "You are mobile robot with two arms."
         system_prompt = system_prompt or base_system_prompt
-        self.llm = init_chat_model(model)
-        self.llm.bind_tools(tools)
+        llm = init_chat_model(model)
+        self.llm = llm.bind_tools(tools)
         self.tools = tools
         self.message_history = [SystemMessage(content=system_prompt)]
         # cameras
         self.main_camera = cv2.VideoCapture(main_camera_usb_port) if main_camera_usb_port else None
+        self.main_camera.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
     def capture_image(self):
         _, frame = self.main_camera.read()
