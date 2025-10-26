1b94be3ec4b2a3a3667823dffcc762fcd1a518e2
Kashyap Puranik <kashthealien@gmail.com>
2025-10-25 18:34:55 -0700

    Use Lerobot SDK instead

--- examples/xlerobot_controller.py
+++ examples/xlerobot_controller.py
@@ -1,36 +1,25 @@
-import sys
-import os
-from robocrew.core.tools import finish_task
 from robocrew.core.LLMAgent import LLMAgent
+from robocrew.core.tools import finish_task
 from robocrew.robots.XLeRobot.tools import create_move_forward, create_turn_left, create_turn_right
 from robocrew.robots.XLeRobot.wheel_controls import XLeRobotWheels
 
-prompt = "You are mobile household robot with two arms."
+# Set up wheels
+wheel_controller = XLeRobotWheels("/dev/ttyACM1")
 
-#set up wheel movement tools
-wheel_arm_usb = "/dev/arm_right"    # provide your right arm usb port, as /dev/TTY0
-wheel_controller = XLeRobotWheels(wheel_arm_usb)
+# Create movement tools
 move_forward = create_move_forward(wheel_controller)
 turn_left = create_turn_left(wheel_controller)
 turn_right = create_turn_right(wheel_controller)
-
-# init agent
+# Create agent
 agent = LLMAgent(
     model="google_genai:gemini-robotics-er-1.5-preview",
-    system_prompt=prompt,
-    tools=[
-        move_forward,
-        turn_left,
-        turn_right,
-        finish_task,
-    ],
-    history_len=4,  # nr of last message-answer pairs to keep
-    main_camera_usb_port="/dev/camera_center",
-    camera_fov=120,
-    sounddevice_index=0,   # index of your microphone sounddevice
+    tools=[move_forward, turn_left, turn_right, finish_task],
+    main_camera_usb_port="/dev/video0",  # provide usb port main camera connected to
+    camera_fov=110,
+    # sounddevice_index=0,  # Your mic device
+    # wakeword="robot",  # The robot listens for this word in your speech
+    history_len=4,
+    debug_mode=False,
 )
-
-print("Agent initialized.")
-
-# run agent
-agent.go()
+agent.task = "Find kitchen in my house and go there."
+agent.go() 
