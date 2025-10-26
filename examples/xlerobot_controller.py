f126db5c114eb3c77dae42248fc1decc38b5d2e9
Kashyap Puranik <kashthealien@gmail.com>
2025-10-25 18:56:05 -0700

    Revert some changes

--- examples/xlerobot_controller.py
+++ examples/xlerobot_controller.py
@@ -3,8 +3,10 @@ from robocrew.core.tools import finish_task
 from robocrew.robots.XLeRobot.tools import create_move_forward, create_turn_left, create_turn_right
 from robocrew.robots.XLeRobot.wheel_controls import XLeRobotWheels
 
-# Set up wheels
-wheel_controller = XLeRobotWheels("/dev/ttyACM1")
+prompt = "You are mobile household robot with two arms."
+#set up wheel movement tools
+wheel_arm_usb = "/dev/ttyACM1"    # provide your right arm usb port, as /dev/TTY0
+wheel_controller = XLeRobotWheels(wheel_arm_usb)
 
 # Create movement tools
 move_forward = create_move_forward(wheel_controller)
@@ -13,13 +15,23 @@ turn_right = create_turn_right(wheel_controller)
 # Create agent
 agent = LLMAgent(
     model="google_genai:gemini-robotics-er-1.5-preview",
-    tools=[move_forward, turn_left, turn_right, finish_task],
+    system_prompt=prompt,
+    tools=[
+        move_forward,
+        turn_left,
+        turn_right,
+        finish_task,
+    ],
     main_camera_usb_port="/dev/video0",  # provide usb port main camera connected to
-    camera_fov=110,
+    camera_fov=120,
     # sounddevice_index=0,  # Your mic device
     # wakeword="robot",  # The robot listens for this word in your speech
     history_len=4,
     debug_mode=False,
 )
+
+print("Agent initialized.")
+
+# run agent with a sample task
 agent.task = "Find kitchen in my house and go there."
 agent.go() 
