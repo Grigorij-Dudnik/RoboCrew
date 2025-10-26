7639ddeb8188c65e97b9214a9db59675f91ea303
Kashyap Puranik <kashthealien@gmail.com>
2025-10-25 18:59:19 -0700

    revert more

--- examples/xlerobot_controller.py
+++ examples/xlerobot_controller.py
@@ -1,18 +1,18 @@
-from robocrew.core.LLMAgent import LLMAgent
 from robocrew.core.tools import finish_task
+from robocrew.core.LLMAgent import LLMAgent
 from robocrew.robots.XLeRobot.tools import create_move_forward, create_turn_left, create_turn_right
 from robocrew.robots.XLeRobot.wheel_controls import XLeRobotWheels
 
 prompt = "You are mobile household robot with two arms."
+
 #set up wheel movement tools
 wheel_arm_usb = "/dev/ttyACM1"    # provide your right arm usb port, as /dev/TTY0
 wheel_controller = XLeRobotWheels(wheel_arm_usb)
-
-# Create movement tools
 move_forward = create_move_forward(wheel_controller)
 turn_left = create_turn_left(wheel_controller)
 turn_right = create_turn_right(wheel_controller)
-# Create agent
+
+# init agent
 agent = LLMAgent(
     model="google_genai:gemini-robotics-er-1.5-preview",
     system_prompt=prompt,
@@ -24,8 +24,8 @@ agent = LLMAgent(
     ],
     main_camera_usb_port="/dev/video0",  # provide usb port main camera connected to
     camera_fov=120,
-    # sounddevice_index=0,  # Your mic device
-    # wakeword="robot",  # The robot listens for this word in your speech
+    sounddevice_index=0,  # Your mic device
+    wakeword="robot",  # The robot listens for this word in your speech
     history_len=4,
     debug_mode=False,
 )
