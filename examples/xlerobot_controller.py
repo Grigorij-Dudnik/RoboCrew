6b0e5982d1bd3c85b7051a88423927963a83ee69
Kashyap Puranik <kashthealien@gmail.com>
2025-11-01 14:31:24 -0700

    Make debug_mode false in example

--- examples/xlerobot_controller.py
+++ examples/xlerobot_controller.py
@@ -26,7 +26,7 @@ agent = LLMAgent(
     main_camera_usb_port="/dev/camera_center",  # provide usb port main camera. Eg: /dev/video0
     camera_fov=120,
     sounddevice_index=2,  # index of your microphone sounddevice
-    debug_mode=True,
+    debug_mode=False,
 )
 
 print("Agent initialized.")
