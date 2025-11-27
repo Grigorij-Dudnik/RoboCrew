ac3dd3b376ce198c4df7049c9ceb2d3e45a5e61f
Grigorij-Dudnik <dudnikgrv@gmail.com>
2025-11-27 16:12:05 +0100

    debug

--- src/robocrew/robots/XLeRobot/tools.py
+++ src/robocrew/robots/XLeRobot/tools.py
@@ -150,5 +150,6 @@ def create_vla_arm_manipulation(
             time.sleep(1)
             main_camera_object.open(main_camera_usb_port)
             main_camera_object.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+            time.sleep(1)  # give some time to re-open camera
 
     return grab_cup
