179528c7eed9cf7eaa5c471f2e498783bd61b1d6
Shinnken <konrad.suchodolski50@gmail.com>
2026-03-15 12:41:54 +0100

    cleanup

--- documentation/.astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_AvA1J from "../../assets/xlerobot-squircle-no-bg.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/xlerobot-squircle-no-bg.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_AvA1J]]);
-		
